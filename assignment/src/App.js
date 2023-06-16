61b948bd35a6463655e35c57d37f7a5e0c39aed5
Kaustubh Nawade <112754493+KaustubhN12@users.noreply.github.com>
2023-06-16 23:33:56 +0530

    env-file-added

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,38 +1,11 @@
-import './App.css';
-import { Octokit } from "octokit";
-import {useEffect} from 'react'
+import { useEffect } from "react";
+import Issues from "./Components/Issues";
 
 function App() {
 
-  const octokit = new Octokit({
-    auth: "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"
-  });
-
- 
-  const githubIssue = async() => {
-    try {
-      const result = await octokit.request("GET /repos/KaustubhN12/dizzy-stitch-9009/issues?state=closed&page=2", {
-          owner: "KaustubhN12",
-          repo: "dizzy-stitch-9009",
-        });
-    
-      const titleAndAuthor = result.data
-
-      console.log(titleAndAuthor)
-    
-    } catch (error) {
-      console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
-    }
-  }
-  useEffect(()=>{
-    githubIssue()
-  },[])
-
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div>
+    <Issues/>
+  </div>;
 }
 
 export default App;
--- assignment/src/Components/Issues.jsx
+++ assignment/src/Components/Issues.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import { Octokit } from "octokit";
 const Issues = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const octokit = new Octokit({
+        auth: process.env.REACT_APP_GIT_OUTH_TOKEN
+      })
 
-export default Issues
+  const githubIssue = async () => {
+    try {
+      const result = await octokit.request(
+        "GET /repos/KaustubhN12/dizzy-stitch-9009/issues?state=closed&page=1",
+        {
+          owner: "KaustubhN12",
+          repo: "dizzy-stitch-9009",
+        }
+      );
+
+      const titleAndAuthor = result.data;
+
+      console.log(titleAndAuthor);
+      console.log(process.env.REACT_APP_GIT_OUTH_TOKEN)
+    } catch (error) {
+      console.log(
+        `Error! Status: ${error.status}. Message: ${error.response.data.message}`
+      );
+    }
+  };
+  useEffect(() => {
+    githubIssue();
+  }, []);
+
+  return <div></div>;
+};
+
+export default Issues;
